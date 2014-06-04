6865a0f7c0f09614c258516d7bfbc0a8d4b6cd38
Samuel Bieri <sbieri@mit.edu>
2014-06-04 18:39:40 +0200

    small error fixed

--- u1real.cpp
+++ u1real.cpp
@@ -263,7 +263,7 @@ int u1real::insert_db()
   os << "P1, dP1, P2, dP2, P3, dP3 ) VALUES (";
 
   os <<  "'', " << N << ", '" << alpha->mylattice->get_desc() << "', '" << pars->desc << "', " << NS << ", " << NF[0] << ", " << (int)pars->apx << ", " << (int)pars->apy << ", " << mc_length << ", " << nk << ", ";
-  os << (int)pars->e2 << (int)pars->TR << ", " << (int)pars->gR << ", ";
+  os << (int)pars->e2 << ", " << (int)pars->TR << ", " << (int)pars->gR << ", ";
 
   os << "round(" << mu[0] << ",3), round(" << pars->ll[2] << ",3), round(" << pars->ll[1] << ",3), ";
   os << "round(" << pars->xi[0] << ",3), round(" << pars->xi[1] << ",3), round(" << pars->xi[2] << ",3), ";
